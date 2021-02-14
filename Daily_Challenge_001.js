65199e8d28df51b9c48f9b4317027dc175fc9710
Nicolas Luevano <nluevano1221@gmail.com>
2021-02-13 19:41:35 -0800

    Daily Challenge 001

--- Daily_Challenge_001.js
+++ Daily_Challenge_001.js
@@ -1,15 +1,15 @@
 /////PROBLEM/////
-Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
-For example,
+// For example,
 
-[true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true]
-The correct answer would be 17.
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
 
 
 

943e30b94f24402efa757250ed0ec333e02a6bb7
Nicolas Luevano <nluevano1221@gmail.com>
2021-02-17 17:03:27 -0800

    003

---
+++ Daily_Challenge_003.js
@@ -0,0 +1,11 @@
+/////PROBLEM/////
+// Given an array of integers, return a new array with each value doubled.
+
+//[1, 2, 3] --> [2, 4, 6]
+
+/////SOLUTION/////
+
+function maps(x){
+    return x.map(item => item*2)
+    }
+    maps([1,2,3])

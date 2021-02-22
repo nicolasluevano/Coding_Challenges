cf7c0e0a28826c17c83e337b001095d452971247
Nicolas Luevano <nluevano1221@gmail.com>
2021-02-21 21:19:20 -0800

    007

---
+++ Daily_Challenge_007.js
@@ -0,0 +1,7 @@
+// Smash Words
+function smash (words) {
+    "use strict";
+    return words.join(' ')
+    
+};
+smash(['hello', 'world', 'this', 'is', 'great'])//returns 'hello world this is great'

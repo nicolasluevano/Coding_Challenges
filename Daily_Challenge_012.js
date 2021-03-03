01b382f0d2c37884dbe86bb9d6eea4ac26adadd4
Nicolas Luevano <nluevano1221@gmail.com>
2021-03-03 14:09:51 -0800

    012

---
+++ Daily_Challenge_012.js
@@ -0,0 +1,12 @@
+//Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+
+
+
+class Cat extends Animal {
+    speak() {
+      return `${this.name} meows.`;
+    }
+  }
+
+  var cat = new Cat('Mr Whiskers');
+  cat.speak()

69ba25021f67fe7edcbaee35111410e3f345f87a
Nicolas Luevano <nluevano1221@gmail.com>
2021-03-16 21:52:55 -0700

    String repeat

---
+++ Daily_Challenge_019.js
@@ -0,0 +1,10 @@
+//Write a function called repeat_string which repeats the given string str exactly count times.
+
+
+
+
+function repeatStr(n,s){
+    
+    console.log(s.repeat(n))
+}
+repeatStr(5,'Hello')

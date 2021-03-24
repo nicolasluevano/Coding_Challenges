0adc1b9d55e1b3f59038df43002932667138d49c
Nicolas Luevano <nluevano1221@gmail.com>
2021-03-23 23:13:36 -0700

    Convert a String to a Number!

---
+++ Daily_Challenge_025.js
@@ -0,0 +1,6 @@
+// We need a function that can transform a string into a number. 
+// all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+var stringToNumber = function(str){
+    return +str;
+  }

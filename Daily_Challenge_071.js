a6bf4237329b8c520dadc457f428aab3421d7440
Nicolas Luevano <nluevano1221@gmail.com>
2021-05-09 22:20:41 -0700

    Abbreviate a Two Word Name

---
+++ Daily_Challenge_071.js
@@ -0,0 +1,20 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// Patrick Feeney => P.F
+
+function abbrevName(name){
+
+    const names = name.split(' ')
+    const initials = []
+    
+    names.forEach(a => initials.push(a[0]))
+      
+    return initials.join('.').toUpperCase()
+        
+    }
